a5919c0f304f64230faa003ed619c70a9cd6294a
Michael Szul <michael@szul.us>
2020-04-22 18:14:00 -0400

    Changes to a clean release/asset upload

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
-# action-release
+# action-release-with-assets
 
-GitHub Action: GitHub Release Action
+GitHub Actions: GitHub Release Action with Asset Attachment
 
-## What this does, and does not do
+## Origins
 
-There are plenty of existing GitHub Actions that handles publish of releases on GitHub. This is yet another.
-
-Use this Action if you have the following workflow:
-
-- All commits to master should build and update a single draft release.
-- When the draft is ready for release, it can be published as prerelease/release manually. It will no longer be updateable.
-- On next commit, another draft release will be created.
+Forked from [Sondre Bjellås](https://github.com/sondreb/action-release) who was using it for draft releases with a manual final release.
 
 ## Usage
 
@@ -21,27 +15,10 @@ Here is an example on how to use this Action:
       uses: sondreb/action-release@master
       with:
         token: ${{ secrets.GITHUB_TOKEN }}
-        files: "action.js;README.md"
-        folder: "${{github.workspace}}/package/"
-        draft: true
-        prerelease: true
-        body: 'This is a pre-release'
-        name: "Draft Release"
+        commitish: dev
+        files: ./release/app.zip
+        prerelease: false
+        body: This is a release
+        name: Draft Release
         tag: v0.0.1
 ```
-
-### Notes
-
-The "prerelease" parameter is not used to allow you to update a published prerelease, but simply mark it by default with prerelease.
-
-This is built as a quick and dirty proof-of-concept during 🎅🎄 and is likely ridled with bugs and problems. Use at your own discretion.
-
-## Releases
-
-0.0.3
-
-- Adds support for folders.
-
-## License
-
-[MIT](LICENSE)
--- action.js
+++ action.js
@@ -1,6 +1,8 @@
+//@ts-check
+
 require('child_process')
     .execSync(
-        'npm install @actions/core @actions/github conventional-changelog-cli mime',
+        'npm install @actions/core @actions/github mime',
         { cwd: __dirname }
     );
 
@@ -11,209 +13,163 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 (async () => {
+    function getFile(filePath) {
+        return {
+            name: path.basename(filePath),
+            mime: mime.getType(filePath) || 'application/octet-stream',
+            size: fs.lstatSync(filePath).size,
+            file: fs.readFileSync(filePath)
+        };
+    }
+
+    async function upload(files, created, release, api) {
+        const file = files.pop();
+
+        if (!file) {
+            return;
+        }
+
+        const fileInfo = getFile(file);
+
+        // If not a new release, we must delete the existing one.
+        if (!created && release.assets) {
+            const asset = release.assets.find(a => a.name === fileInfo.name);
+
+            // If the asset already exists, make sure we delete it first.
+            if (asset) {
+                var assetOptions = {
+                    ...github.context.repo,
+                    asset_id: asset.id
+                };
+
+                console.log(`Asset ${ fileInfo.name } already exists, it must be put in a 🕳️.`);
+                console.log(`Asset Options (for delete operation) ${ assetOptions }`);
+
+                try {
+                    const result = await api.repos.deleteReleaseAsset(assetOptions);
+                    console.log(`Result from delete ${ result }`);
+                }
+                catch (err) {
+                    console.error(`⚠️ Failed to delete file ${ fileInfo.name }: ${ err }`);
+                }
+            }
+        }
+
+        console.log(`🚧 Uploading ${ fileInfo.name }.`);
+
+        try {
+            const result = await api.repos.uploadReleaseAsset({
+                url: release.upload_url,
+                headers: {
+                    ['content-type']: fileInfo.mime,
+                    ['content-length']: fileInfo.size
+                },
+                name: fileInfo.name,
+                file: fileInfo.file
+            });
+
+            console.log(`Result from upload' ${ result }`);
+        }
+        catch (error) {
+            console.error(`⚠️ Failed to upload file  ${ error }`);
+        }
+
+        // Recursive go through all files to upload as release assets.
+        await upload(files, created, release, api);
+    }
+
     try {
         const api = new github.GitHub(core.getInput('token'));
         const tag = core.getInput('tag');
+        const commit = core.getInput('commitish');
         const name = core.getInput('name');
         const body = core.getInput('body');
-        const verbose = core.getInput('verbose') == 'true'; // input is always string, not boolean.
-        const draft = core.getInput('draft') == 'true';
+        const draft = true;
         const prerelease = core.getInput('prerelease') == 'true';
-        let files = null;
+        const files = [];
 
-        if (core.getInput('folder'))
-        {
-            const folder = core.getInput('folder');
-            log('Reading files in folder:' + folder);
-
-            files = fs.readdirSync(folder, {withFileTypes: true})
-            .filter(item => !item.isDirectory())
-            .map(item => path.join(folder, item.name))
-
-            log('Found files: ', files);
+        if(files.indexOf(';') !== -1) {
+            const fss = core.getInput('files').split(';');
+            files.push(...fss)
         }
-        else
-        {
-            files = core.getInput('files').split(';');
+        else {
+            files.push(core.getInput('files'));
         }
 
-        const commit = 'master'; // This could likely be a parameter in the future. Get commit like this: github.context.sha
         let release = null;
         let created = false; // Indicate if the release was created, or merely updated.
 
-        function info(text, ...params) {
-            log(text, true, ...params);
-        }
-
-        function debug(text, ...params) {
-            log(text, verbose, ...params);
-        }
-
-        function log(text, enabled, ...params) {
-            if (enabled) {
-                console.log(text, ...params);
-            }
-        }
-
-        function getFile(filePath) {
-
-            log('getFile: ' + filePath);
-
-            return {
-                name: path.basename(filePath),
-                mime: mime.getType(filePath) || 'application/octet-stream',
-                size: fs.lstatSync(filePath).size,
-                file: fs.readFileSync(filePath)
-            }
-        }
-
-        info(`🎄 <- That is when I wrote this code.`);
+        
 
         // First let us try to get the release.
         try {
-            result = await api.repos.getReleaseByTag({
+            const result = await api.repos.getReleaseByTag({
                 ...github.context.repo,
                 tag: tag
             });
 
-            debug(`Release already exists. Do the 🐹 dance.`, result);
-
             // If this has been published, we'll create a new release.
             if (draft && !result.data.draft) {
                 release = null;
-                debug(`The existing release was not draft. We can create a brand ✨ new release.`);
+                console.log(`The existing release was not draft. We can create a brand ✨ new release.`);
             }
             else {
                 // We cannot update assets on existing releases, so until a future update, we'll ignore updating releases that are published.
-                info(`Draft parameter is set to false and there is an existing release. Skipping any updates to release 🛑.`);
+                console.log(`Draft parameter is set to false and there is an existing release. Skipping any updates to release 🛑.`);
                 return;
             }
         }
-        catch (error) {
-            if (error.name != 'HttpError' || error.status != 404) {
-                throw error;
-            }
-        }
-
-        // Get releases if the first release get was not satisfactory.
-        if (!release) {
-            try {
-                var releases = await api.repos.listReleases({
-                    ...github.context.repo
-                });
-
-                debug('Releases', releases);
-
-                for (var i = 0; i < releases.data.length; ++i) {
-                    var r = releases.data[i];
-
-                    if (r.tag_name == tag && r.draft == draft && r.prerelease == prerelease) {
-                        release = r;
-                        debug('Found existing release based on searching.');
-                        break;
-                    }
-                }
-            }
-            catch (error) {
-                if (error.name != 'HttpError' || error.status != 404) {
-                    throw error;
-                }
+        catch (e) {
+            if (e.name != 'HttpError' || e.status != 404) {
+                throw e;
             }
         }
 
         // Define the options, these are almost same when creating new and updating existing.
-        var releaseOptions = {
+        const releaseOptions = {
             ...github.context.repo,
             tag_name: tag,
             target_commitish: commit,
-            name,
-            body,
+            name: name,
+            body: body,
             prerelease: prerelease,
             draft: draft
         };
 
         // Create a release if it doesn't already exists.
         if (!release) {
-            debug('Release Options (Create)', releaseOptions);
-            info(`🌻 Creating GitHub release for tag "${tag}".`);
+            console.log(`Release Options (Create) ${ releaseOptions }`);
+            console.log(`🌻 Creating GitHub release for tag ${ tag }.`);
 
             const result = await api.repos.createRelease(releaseOptions);
             release = result.data;
+            //@ts-ignore
+            releaseOptions.release_id = release.id
             created = true;
         }
         else {
+            //@ts-ignore
             releaseOptions.release_id = release.id; // Must be part of the parameters.
 
-            debug('Release Options (Update)', releaseOptions);
-            info(`Found The 🦞. Updating GitHub release for tag "${tag}".`);
+            console.log(`Release Options (Update) ${ releaseOptions }`);
+            console.log(`Found the 🍺. Updating GitHub release for tag ${ tag }.`);
 
+            //@ts-ignore
             const result = await api.repos.updateRelease(releaseOptions);
             release = result.data;
         }
 
-        async function upload() {
-            var file = files.pop();
-
-            if (!file) {
-                return;
-            }
-
-            var fileInfo = getFile(file);
-
-            // If not a new release, we must delete the existing one.
-            if (!created && release.assets) {
-                const asset = release.assets.find(a => a.name === fileInfo.name);
-
-                // If the asset already exists, make sure we delete it first.
-                if (asset) {
-                    var assetOptions = {
-                        ...github.context.repo,
-                        asset_id: asset.id
-                    };
-
-                    info(`Asset "${fileInfo.name}" already exists, it must be put in a 🕳️.`);
-                    debug('Asset Options (for delete operation)', assetOptions);
-
-                    try {
-                        const result = await api.repos.deleteReleaseAsset(assetOptions);
-                        debug('Result from delete', result);
-                    }
-                    catch (err) {
-                        console.error(`⚠️ Failed to delete file "${fileInfo.name}"`, err);
-                    }
-                }
-            }
-
-            info(`🚧 Uploading ${fileInfo.name}.`);
-
-            try {
-                const result = await api.repos.uploadReleaseAsset({
-                    url: release.upload_url,
-                    headers: {
-                        ['content-type']: fileInfo.mime,
-                        ['content-length']: fileInfo.size
-                    },
-                    name: fileInfo.name,
-                    file: fileInfo.file
-                });
-
-                debug('Result from upload', result);
-            }
-            catch (error) {
-                console.error(`⚠️ Failed to upload file`, error);
-            }
-
-            // Recursive go through all files to upload as release assets.
-            await upload();
-        }
-
         // Start uploading all specified files.
-        await upload();
+        await upload(files, created, release, api);
 
-        info('All is norminal 🚀. Execution has ended.')
+        const updatedOptions = { ...releaseOptions , ...{ draft: false }};
+        //@ts-ignore
+        await api.repos.updateRelease(updatedOptions);
 
-    } catch (error) {
-        console.error(error);
-        core.setFailed(error.message);
+        console.log('All is normal 🚀. Execution has ended.')
+
+    } catch (e) {
+        console.error(`GitHub Action has failed: ${ e }`);
+        core.setFailed(e.message);
     }
 })();
